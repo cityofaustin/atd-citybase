4e849f0fe8cdd726f2f0f07b2693eac22a6c892f
chiaberry <analuciaby@gmail.com>
2025-07-30 17:31:16 -0500

    splitting out the field maps and adding back UAT for banners

--- utils/field_maps.py
+++ utils/field_maps.py
@@ -1,30 +1,83 @@
 # map knack field ids to human rememberable names
-FIELD_MAPS = {
+
+STREET_BANNER_TRANSACTION_REFUND_PRODUCTION = {
     "total_amount": "field_3342",
-    "payment_status": "field_3353",
     "invoice_id": "field_3327",
     "created_date": "field_3320",
-    "transaction_paid_date": "field_3352",
-    "messages_invoice_id": "field_3363", # is this different in staging?
-    "messages_connected_invoice": "field_3369",
+    "customer_name": "field_3334",
+    "event_name": "field_3336",
+    "type": "field_3333",
+    "banner_reservations_lpb": "field_3328",
+    "banner_reservations_ots": "field_3329",
+    "sub_description": "field_3351",
+}
+
+STREET_BANNER_TRANSACTION_REFUND_DEVELOPMENT = {
+    "total_amount": "field_3338",
+    "invoice_id": "field_3333",
+    "created_date": "field_3320",
+    "customer_name": "field_3334",
+    "event_name": "field_3348",
+    "type": "field_3337",
+    "banner_reservations_lpb": "field_3326",
+    "banner_reservations_ots": "field_3327",
+    "sub_description": "field_3349",
+}
+
+STREET_BANNER_MESSAGES_OBJECT_ID = "object_181"
+
+STREET_BANNER_MESSAGES_PRODUCTION = {
+    "messages_invoice_id": "field_3363",
+    "messages_connected_invoice": "field_3369", # invoice_transaction
     "messages_created_date": "field_3366",
     "messages_status": "field_3361",
     "messages_citybase_id": "field_3368",
-    "ots_connection_field": "field_3329",
-    "ots_application_status": "field_2862",  # in banner_reservations object
-    "ots_paid_status": "field_2858",  # in banner_reservations object THIS IS CALLED PAYMENT RECEIVED
-    "ots_payment_date": "field_3144",  # in banner_reservations object
-    "lpb_connection_field": "field_3328",
-    "lpb_application_status": "field_2796",  # in banner_reservations object
-    "lpb_paid_status": "field_2808",  # in banner_reservations object
-    "lpb_payment_date": "field_2809",  # in banner_reservations object
 }
 
-REFUND_FIELDS = {
-        "customer_name": "field_3334",
-        "event_name": "field_3336",
-        "type": "field_3333",
-        "banner_reservations_lpb": "field_3328",
-        "banner_reservations_ots": "field_3329",
-        "sub_description": "field_3351",
+STREET_BANNER_MESSAGES_DEVELOPMENT = {
+    "messages_invoice_id": "field_3333",
+    "messages_connected_invoice": "field_3372", # invoice_transactions_banner
+    "messages_created_date": "field_3369",
+    "messages_status": "field_3367",
+    "messages_citybase_id": "field_3378",
+}
+
+STREET_BANNER_TRANSACTIONS_OBJECT_ID = "object_180"
+
+STREET_BANNER_TRANSACTIONS_PRODUCTION = {
+    "payment_status": "field_3353", # called transaction status in knack #TODO: rename to match knack
+    "transaction_paid_date": "field_3352",
+}
+
+STREET_BANNER_TRANSACTIONS_DEVELOPMENT = {
+    "payment_status": "field_3352", # called transaction status in knack #TODO: rename to match knack
+    "transaction_paid_date": "field_3366",
+}
+
+OTS_OBJECT_ID = "object_164"
+
+OVER_THE_STREET_PRODUCTION = {
+    "ots_application_status": "field_2862",
+    "ots_paid_status": "field_2858",  #  THIS IS CALLED PAYMENT RECEIVED
+    "ots_payment_date": "field_3144",
+}
+
+OVER_THE_STREET_DEVELOPMENT = {
+    "ots_application_status": "field_2862",
+    "ots_paid_status": "field_2858",  #  THIS IS CALLED PAYMENT RECEIVED
+    "ots_payment_date": "field_3144",
+}
+
+LPB_OBJECT_ID = "object_161"
+
+LAMPPOST_PRODUCTION = {
+    "lpb_application_status": "field_2796",
+    "lpb_paid_status": "field_2808",  #  THIS IS CALLED PAYMENT RECEIVED
+    "lpb_payment_date": "field_2809",
+}
+
+LAMPPOST_DEVELOPMENT = {
+    "lpb_application_status": "field_2796",
+    "lpb_paid_status": "field_2808",  #  THIS IS CALLED PAYMENT RECEIVED
+    "lpb_payment_date": "field_2809",
 }
