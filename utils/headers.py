b25fdb5c66906e7f8c5b806052f6ffdff6cc2cfe
chiaberry <analuciaby@gmail.com>
2025-09-05 14:16:30 -0500

    dry up the headers

--- utils/headers.py
+++ utils/headers.py
@@ -1,21 +1,20 @@
 import os
 
-KNACK_STREET_BANNER_APP_ID = os.getenv("KNACK_STREET_BANNER_APP_ID")
-KNACK_STREET_BANNER_API_KEY = os.getenv("KNACK_STREET_BANNER_API_KEY")
-KNACK_SMART_MOBILITY_APP_ID = os.getenv("KNACK_SMART_MOBILITY_APP_ID")
-KNACK_SMART_MOBILITY_API_KEY = os.getenv("KNACK_SMART_MOBILITY_API_KEY")
+KNACK_APP_KEYS = {
+    "STREET_BANNER": {
+        "app_id": os.getenv("KNACK_STREET_BANNER_APP_ID"),
+        "api_key": os.getenv("KNACK_STREET_BANNER_API_KEY"),
+    },
+    "SMART_MOBILITY": {
+        "app_id": os.getenv("KNACK_SMART_MOBILITY_APP_ID"),
+        "api_key": os.getenv("KNACK_SMART_MOBILITY_API_KEY"),
+    },
+}
 
 def knack_headers(knack_app):
-    if knack_app == "STREET_BANNER":
-        headers = {
-            "X-Knack-Application-Id": KNACK_STREET_BANNER_APP_ID,
-            "X-Knack-REST-API-Key": KNACK_STREET_BANNER_API_KEY,
-            "Content-Type": "application/json",
-        }
-    elif knack_app == "SMART_MOBILITY":
-        headers = {
-            "X-Knack-Application-Id": KNACK_SMART_MOBILITY_APP_ID,
-            "X-Knack-REST-API-Key": KNACK_SMART_MOBILITY_API_KEY,
-            "Content-Type": "application/json",
-        }
+    headers = {
+        "X-Knack-Application-Id": KNACK_APP_KEYS.get(knack_app).get("app_id"),
+        "X-Knack-REST-API-Key": KNACK_APP_KEYS.get(knack_app).get("api_key"),
+        "Content-Type": "application/json",
+    }
     return headers
